3c222923f6704fc3b71617bf0de48b3521f41a33
satheeshms <satheeshms@gmail.com>
2021-03-04 16:06:20 +0530

    new new katas :)

--- src/main/java/com/satz/kata/BookUtil.java
+++ src/main/java/com/satz/kata/BookUtil.java
@@ -1,6 +1,9 @@
 package com.satz.kata;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.satz.kata.pojo.Book;
 
@@ -14,5 +17,10 @@ public class BookUtil {
     public static List<Book> getAllBooksCostsMoreThan(List<Book> books, int cost){
 
         return List.of();
+    }
+
+    public static Map<String, List<Book>> getAllBooksMappedToAuthor(List<Book> books){
+
+        return Map.of();
     }
 }
--- src/test/java/com/satz/kata/BookUtilTest.java
+++ src/test/java/com/satz/kata/BookUtilTest.java
@@ -1,9 +1,12 @@
 package com.satz.kata;
 
 import static com.satz.kata.BookUtil.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Map;
 
+import com.google.common.collect.Maps;
 import com.satz.kata.pojo.Book;
 
 import org.junit.jupiter.api.Test;
@@ -13,11 +16,11 @@ public class BookUtilTest {
     Book b1 = new Book("Java Complete Reference", "Herb Schildt", 499);
     Book b2 = new Book("Design Patterns", "Eric Gamma", 399);
     Book b3 = new Book("Effective Java", "Joshua Bloch", 299);
-    
+
     Book b4 = new Book("Java Concurrency in Practice", "Joshua Bloch", 199);
 
     @Test
-    public void getAllBookNamesTest(){
+    public void mapping(){
        
         List<Book> books = List.of(b1, b2, b3);
         List<String> names = getAllBookNames(books);
@@ -27,12 +30,25 @@ public class BookUtilTest {
     }
 
     @Test
-    public void getAllBooksCostsMoreThanAValue(){
+    public void filter(){
         List<Book> books = List.of(b1, b2, b3);
         List<Book> costly = getAllBooksCostsMoreThan(books, 300);
 
         List<Book> expected = List.of(b1,b2);
         Assertions.assertTrue(expected.stream().allMatch(s -> costly.contains(s)));
+    }
+
+    @Test
+    public void grouping(){
+        List<Book> books = List.of(b1, b2, b3, b4);
+        Map<String, List<Book>> authors =  getAllBooksMappedToAuthor(books);
+
+        Map<String, List<Book>> expected = Map.of(
+                         b1.getAuthor(), List.of(b1)
+                        ,b2.getAuthor(), List.of(b2)
+                        ,b3.getAuthor(), List.of(b3,b4));
+        
+        assertTrue(Maps.difference(expected, authors).areEqual());
     }
 
 }
